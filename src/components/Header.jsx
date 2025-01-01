a94ad6e4e969d88d0a89a3db2792c18162b4ecff
azikenp <azikenp26@gmail.com>
2025-01-01 16:48:13 +0100

    update index.css

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,43 @@
+import {
+  HiHome,
+  HiMagnifyingGlass,
+  HiStar,
+  HiPlayCircle,
+  HiTv,
+} from "react-icons/hi2";
+
+import { HiPlus, HiDotsVertical } from "react-icons/hi";
 import logo from "../assets/Images/logo.png";
+import HeaderItem from "./HeaderItem";
 
 const Header = () => {
+  const menu = [
+    {
+      name: "HOME",
+      icon: HiHome,
+    },
+    {
+      name: "SEARCH",
+      icon: HiMagnifyingGlass,
+    },
+    {
+      name: "WATCH LIST",
+      icon: HiPlus,
+    },
+    {
+      name: "ORIGINALS",
+      icon: HiStar,
+    },
+    {
+      name: "MOVIES",
+      icon: HiPlayCircle,
+    },
+    {
+      name: "SERIES",
+      icon: HiTv,
+    },
+  ];
+
   return (
     <div>
       <img
@@ -8,6 +45,9 @@ const Header = () => {
         className="w-[80px] md:w-[115px] object-cover"
         alt="logo"
       />
+      {menu.map((item) => (
+        <HeaderItem name={item.name} Icon={item.icon} />
+      ))}
     </div>
   );
 };
