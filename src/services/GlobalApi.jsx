078966435bf60b44ae25eeccc8ee3f07ba0da1b8
Azikenp <azikenp26@gmail.com>
2025-01-04 21:00:50 +0100

    globalApi modified

--- src/services/GlobalApi.jsx
+++ src/services/GlobalApi.jsx
@@ -4,8 +4,7 @@ const movieBaseUrl = "https://api.themoviedb.org/3";
 
 const api_key = import.meta.env.VITE_API_KEY;
 
-const movieByGenreBaseURL =
-  "https://api.themoviedb.org/3/discover/movie?api_key=2ec0d66f5bdf1dd12eefa0723f1479cf";
+const movieByGenreBaseURL = `https://api.themoviedb.org/3/discover/movie?api_key=${api_key}`;
 
 //https://api.themoviedb.org/3/trending/all/day?api_key=2ec0d66f5bdf1dd12eefa0723f1479cf
 const getTrendingVideos = axios.get(
