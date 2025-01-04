30823c2e8806291e35402c6bcfb3d0fc687c9733
Azikenp <azikenp26@gmail.com>
2025-01-04 20:52:10 +0100

    video overlay rendered

--- src/components/ProductionHouse.jsx
+++ src/components/ProductionHouse.jsx
@@ -41,9 +41,21 @@ function ProductionHouse() {
   ];
   return (
     <div className="flex gap-2 md:gap-5 p-2 px-5 md:px-16">
-      {productionHouseList.map((item, index) => (
-        <div key={index} className="border-[2px] border-gray-600 rounded-lg">
-          <img src={item.image} className="w-full" />
+      {productionHouseList.map((item) => (
+        <div
+          key={item.id}
+          className="relative border-[2px] border-gray-600 rounded-lg hover:scale-110 transition-all duration-300 ease-in-out cursor-pointer"
+        >
+          <img src={item.image} className="w-full z-[1]" />
+          <video
+            src={item.video}
+            autoPlay
+            loop
+            playsInline
+            muted
+            className="absolute z-0 top-0 rounded-md 
+            opacity-0 hover:opacity-50"
+          />
         </div>
       ))}
     </div>
