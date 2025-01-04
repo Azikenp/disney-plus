d0ac22715c9a2abe1b538201037c3c754ab97553
Azikenp <azikenp26@gmail.com>
2025-01-04 20:34:51 +0100

    slider done

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,10 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import GlobalApi from '../services/GlobalApi'
+import { HiChevronLeft, HiChevronRight } from 'react-icons/hi';
 
 const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/original";
+const screenWidth = window.innerWidth;
 
 const Slider = () => {
-    const [movieList, setMovieList] = useState([])
+    const [movieList, setMovieList] = useState([]);
+    const elementRef = useRef();
 
     useEffect(() => { getTrendingMovies() }, [])
 
@@ -15,16 +18,30 @@ const Slider = () => {
         })
     }
 
+    const sliderRight = element => {
+        element.scrollLeft += screenWidth - 110
+    }
+    const sliderLeft = element => {
+        element.scrollLeft -= screenWidth - 110
+    }
 
-    return (
-        <div className='flex overflow-x-auto w-full px-16 py-4 scrollbar-hide'>
+
+    return (<div>
+        <HiChevronLeft className='hidden md:block cursor-pointer text-white text-[30px] absolute mx-8 mt-[150px]' onClick={() => sliderLeft(elementRef.current)} />
+        <HiChevronRight className='hidden md:block cursor-pointer text-white text-[30px] absolute mx-8 mt-[150px] right-0' onClick={() => sliderRight(elementRef.current)} />
+        <div
+            className='flex overflow-x-auto w-full px-16 py-4 scrollbar-hide scroll-smooth'
+            ref={elementRef}>
             {
                 movieList.map((item, index) => (
-                    < img key={index} src={IMAGE_BASE_URL + item.backdrop_path} className='min-w-full h-[310px] object-cover object-left-top mr-5 rounded-md' />
+                    < img
+                        key={index}
+                        src={IMAGE_BASE_URL + item.backdrop_path} className='min-w-full md:h-[310px] object-cover object-left-top mr-5 rounded-md hover:border-[4px] border-gray-400 transition-all duration-100 ease-in' />
                 )
                 )
             }
-        </div>
+        </div></div>
+
     )
 }
 
